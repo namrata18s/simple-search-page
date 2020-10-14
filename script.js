2e03712db36a68fb0577f01ad3bcdf715c09774f
ajay <ajay17599@gmail.com>
2020-10-15 00:17:12 +0530

    Added the clear button in the search bar

---
+++ script.js
@@ -0,0 +1,15 @@
+Array.prototype.forEach.call(document.querySelectorAll('.clearable-input'), function(el) {
+  var input = el.querySelector('input');
+
+  conditionallyHideClearIcon();
+  input.addEventListener('input', conditionallyHideClearIcon);
+  el.querySelector('[data-clear-input]').addEventListener('click', function(e) {
+    input.value = '';
+    conditionallyHideClearIcon();
+  });
+
+  function conditionallyHideClearIcon(e) {
+    var target = (e && e.target) || input;
+    target.nextElementSibling.style.display = target.value ? 'block' : 'none';
+  }
+});
